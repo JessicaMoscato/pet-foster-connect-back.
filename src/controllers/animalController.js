1e6d90a60d322d1368ebb3f3e4f354fd210b91e3
Charlottes27-charlotte-bec <bec.charlotte@yahoo.fr>
2024-10-30 16:26:08 +0100

    begin create animal's controller

---
+++ src/controllers/animalController.js
@@ -0,0 +1,21 @@
+import { Animal } from '../models/index.js';
+
+export const animalController = {
+    getAll: async (req, res) => {
+        const animals = await Animal.findAll();
+        res.json(animals);
+    },
+
+    getOne: async (req, res) => {
+        const animalId = req.params.id;
+        const animal = await Animal.findbyPk(animalId);
+
+        // gestion d'une 404 ici?
+
+        res.json(animal);
+    },
+
+    createOne: async (req, res) => {
+        
+    }
+};
