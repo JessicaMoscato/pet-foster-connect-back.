285d899d3d828a2bf131ea6967534e517bfb8c13
JessicaMoscato <jessica.moscato@oclock.school>
2024-10-31 09:59:41 +0100

    refactor router files

--- src/routers/askRouter.js
+++ src/routers/askRouter.js
@@ -1,11 +1,11 @@
 //! Router secondaire pour les routes liées aux alertes (prefixe de route : /api/ask)
-//! Toutes les routes définies ici auront automatiquement le préfixe /api/ask
+
 import { Router } from "express";
-import { askController } from "../controllers/askController.js"; /* controlleur et chemin à vérifier */
+import { askController } from "../controllers/askController.js"; 
 
 export const router = Router();
 
-/* Vérifier le nom des méthodes */
+
 router.get("/", askController.getAllAsks); // Route pour lister toutes les demandes (de l'association)
 router.get("/:id", askController.getAskById); // Route pour obtenir le détail d'une demande
 router.post ("/", askController.createAsk); // Route pour créer une nouvelle demande
