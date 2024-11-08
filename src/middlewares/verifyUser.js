b08b298d84d822fb21ebe2864e2c49a11e9c6e36
Charlottes27-charlotte-bec <bec.charlotte@yahoo.fr>
2024-11-08 18:11:36 +0100

    create frist's version of verifyUser

--- src/middlewares/verifyUser.js
+++ src/middlewares/verifyUser.js
@@ -2,9 +2,10 @@ import {Family} from "../models/index.js";
 
 export function verifyUser () {
     return async function (req, res, next) {
+        const familyId = req.params.id
+        const family = await Family.findByPk(familyId)
         const familyUser = await family.getUser();
-        console.log(familyUser);
-        if (familyUser !== req.user.id) {
+        if (familyUser.id !== req.user.id) {
             return res.status(403).json({
                 error: "Accès interdit: Vous n'etes pas habilité"
             });
