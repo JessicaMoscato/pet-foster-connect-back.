e3b2dbedab7ef692bd2e9a4584d4ba6e2fbec50c
Charlottes27-charlotte-bec <bec.charlotte@yahoo.fr>
2024-11-11 17:10:23 +0100

    create verifyAsso for router patch with good user's profil

--- src/routers/associationRouter.js
+++ src/routers/associationRouter.js
@@ -7,6 +7,7 @@ import { verifyToken } from "../auth/verifyToken.js";
 import { isRoleAuthorizedMiddleware } from "../middlewares/rightsMiddleware.js";
 import { validate } from "../validation/validate.js"; // Importation de la fonction de validation
 import { patchSchema } from "../validation/patchAssociation.js"; // Importation du schéma de modification d'utilisateur JOI
+import { verifyAssociation } from "../middlewares/verifyUser.js";
 
 
 export const router = Router();
@@ -15,5 +16,5 @@ export const router = Router();
 router.get("/", withTryCatch(associationController.getAllAssociations)); // Route pour lister toutes les associations
 router.get("/:id", withTryCatch(associationController.getAssociationById)); // Route pour obtenir le détail d'une association
 
-router.patch("/:id",verifyToken, isRoleAuthorizedMiddleware(["association"]), validate(patchSchema, "body"), withTryCatch(associationController.patchAssociation));
+router.patch("/:id",verifyToken, isRoleAuthorizedMiddleware(["association"]),verifyAssociation(), validate(patchSchema, "body"), withTryCatch(associationController.patchAssociation));
 router.delete("/:id",verifyToken, isRoleAuthorizedMiddleware(["association"]), withTryCatch(associationController.deleteAssociation))
