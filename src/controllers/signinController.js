163be42fcf8b8db575ca30ca523487c862607c33
Charlottes27-charlotte-bec <bec.charlotte@yahoo.fr>
2024-11-12 10:32:30 +0100

    modification for delete space before or after mail and password for connexion

--- src/controllers/signinController.js
+++ src/controllers/signinController.js
@@ -3,20 +3,13 @@ import { Scrypt } from "../auth/Scrypt.js";
 import { generateToken } from "../auth/tokenService.js";
 import validator from "validator";
 
-//! Fonction de validation de mot de passe
-const validatePassword = (password) => {
-  const hasUpperCase = /[A-Z]/.test(password); // Vérifie la présence d'une majuscule
-  const hasSpecialChar = /[!@#$%^&*]/.test(password); // Vérifie la présence d'un caractère spécial
-  const hasNumber = /\d/.test(password); // Vérifie la présence d'un chiffre
-  const isValidLength = password.length >= 8; // Vérifie que la longueur minimale est de 8 caractères
-
-  return hasUpperCase && hasSpecialChar && hasNumber && isValidLength;
-};
-
 export const signinController = {
   //! Méthode pour connecter un utilisateur
   async signinUser(req, res) {
-    const { email, password } = req.body;
+    let { email, password } = req.body;
+
+    email = email.trim();
+    password = password.trim();
 
     // Vérification de la présence de l'email et du mot de passe
     if (!email || !password) {
@@ -32,13 +25,13 @@ export const signinController = {
 
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(401).json({ message: "Utilisateur non trouvé" });
+      return res.status(401).json({ message: "Identifiant ou Mot de passe incorrect" });
     }
 
-    // Vérification du mot de passe
+    // Vérification du mot de passe et de l'email
     const isValidPassword = await Scrypt.compare(password, user.password);
     if (!isValidPassword) {
-      return res.status(401).json({ message: "Mot de passe incorrect" });
+      return res.status(401).json({ message: "Identifiant ou Mot de passe incorrect" });
     }
 
     // Génération du token JWT
